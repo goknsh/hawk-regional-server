f6ff38c75e8947d3969ac3c58de26d64c5d19b54
Akaanksh Raj <akaankshraj@gmail.com>
2018-04-04 02:19:58 +0000

    first commit

---
+++ api/v2/data.php
@@ -0,0 +1,65 @@
+<?php
+    $dourl = $_GET["url"] . '?'. md5(uniqid(rand(), true)) . '=' . md5(uniqid(rand(), true));
+    error_reporting(0);
+    header("Access-Control-Allow-Origin: *");
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_URL, $dourl);
+    curl_setopt($c, CURLOPT_HEADER, TRUE);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($c, CURLOPT_FRESH_CONNECT, 1);
+    curl_setopt($c, CURLOPT_CERTINFO, TRUE);
+    curl_setopt($c, CURLOPT_TIMEOUT, 15);
+    curl_setopt($c, CURLOPT_MAX_RECV_SPEED_LARGE, 10000);
+    curl_setopt($c, CURLOPT_COOKIESESSION, TRUE);
+    curl_setopt($c, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($c, CURLOPT_MAXREDIRS, 15);
+    curl_setopt($c, CURLOPT_FORBID_REUSE, TRUE);
+    curl_setopt($c, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; Ping Bot/1.0; +https://useping.ga/bot)');
+    
+    if (!curl_exec($c) | (int)curl_getinfo($c)['http_code'] === 408) {
+        $return = array(
+            "1" => "timeout",
+            "2" => (curl_getinfo($c)['namelookup_time']+curl_getinfo($c)['connect_time']+curl_getinfo($c)['appconnect']+curl_getinfo($c)['pretransfer_time']+curl_getinfo($c)['redirect_time']+curl_getinfo($c)['starttransfer_time'])*1000,
+            "3" => curl_getinfo($c)['http_code'],
+            "4" => curl_getinfo($c)['speed_download'] * 8 / 1024 / 1024,
+            "5" => curl_getinfo($c)['size_download'],
+            "6" => curl_getinfo($c)['namelookup_time']*1000,
+            "7" => curl_getinfo($c)['connect_time']*1000,
+            "8" => explode("CN = ", curl_getinfo($c)['certinfo'][1]['Subject'])[1],
+            "9" => curl_getinfo($c)['certinfo'][0]['Expire date']
+        );
+        curl_close($c);
+        echo json_encode($return);
+    } else {
+        $code = (int)curl_getinfo($c)['http_code'];
+        if ($code < 399) {
+            $return = array(
+                "1" => "up",
+                "2" => (curl_getinfo($c)['namelookup_time']+curl_getinfo($c)['connect_time']+curl_getinfo($c)['appconnect']+curl_getinfo($c)['pretransfer_time']+curl_getinfo($c)['redirect_time']+curl_getinfo($c)['starttransfer_time'])*1000,
+                "3" => curl_getinfo($c)['http_code'],
+                "4" => curl_getinfo($c)['speed_download'] * 8 / 1024 / 1024,
+                "5" => curl_getinfo($c)['size_download'],
+                "6" => curl_getinfo($c)['namelookup_time']*1000,
+                "7" => curl_getinfo($c)['connect_time']*1000,
+                "8" => explode("CN = ", curl_getinfo($c)['certinfo'][1]['Subject'])[1],
+                "9" => curl_getinfo($c)['certinfo'][0]['Expire date'],
+            );
+            curl_close($c);
+            echo json_encode($return);
+        } else {
+            $return = array(
+                "1" => "down",
+                "2" => (curl_getinfo($c)['namelookup_time']+curl_getinfo($c)['connect_time']+curl_getinfo($c)['appconnect']+curl_getinfo($c)['pretransfer_time']+curl_getinfo($c)['redirect_time']+curl_getinfo($c)['starttransfer_time'])*1000,
+                "3" => curl_getinfo($c)['http_code'],
+                "4" => curl_getinfo($c)['speed_download'] * 8 / 1024 / 1024,
+                "5" => curl_getinfo($c)['size_download'],
+                "6" => curl_getinfo($c)['namelookup_time']*1000,
+                "7" => curl_getinfo($c)['connect_time']*1000,
+                "8" => explode("CN = ", curl_getinfo($c)['certinfo'][1]['Subject'])[1],
+                "9" => curl_getinfo($c)['certinfo'][0]['Expire date'],
+            );
+            curl_close($c);
+            echo json_encode($return);
+        }
+    }
